4fb62af2b2951c0204bbb7aa2baaa9bddfdbefb9
Frans de Boer <frans-de-boer@hotmail.com>
2022-07-20 15:52:49 +0200

    basic ban list creation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import uuid
 
 from discord.ext import commands
 from discord.ext.commands.context import Context
@@ -15,17 +16,56 @@ cur = conn.cursor()
 @bot.event
 async def on_ready():
     cur.execute('''CREATE TABLE IF NOT EXISTS ban_lists (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER,
-            guild_id INTEGER
+            id        VARCHAR(8) PRIMARY KEY UNIQUE        NOT NULL,
+            user_id   INTEGER                              NOT NULL,
+            guild_id  INTEGER                              NOT NULL,
+            timestamp TIMESTAMP  DEFAULT CURRENT_TIMESTAMP NOT NULL
         );''')
-
+    print('Initialisation done')
 
 
 async def get_banned_list(ctx: Context):
     guild: Guild = ctx.guild
     bans: list[BanEntry] = await guild.bans()
     return bans
+
+
+@bot.command()
+async def createList(ctx: Context):
+    bans = await get_banned_list(ctx)
+
+    ban_list_id = str(uuid.uuid4())[:8]
+    user_id = ctx.author.id
+    guild_id = ctx.guild.id
+
+    cur.execute('INSERT INTO ban_lists (id, user_id, guild_id) VALUES (?, ?, ?)',
+                (ban_list_id, user_id, guild_id))
+    conn.commit()
+
+    await ctx.send(f'User {ctx.author} created ban list ({ban_list_id}) for server {ctx.guild}')
+
+
+@bot.command()
+async def viewLists(ctx: Context):
+    user_id = ctx.author.id
+    guild_id = ctx.guild.id
+
+    cur.execute('SELECT * FROM ban_lists WHERE user_id = ?', (user_id, ))
+    user_lists = cur.fetchall()
+    cur.execute('SELECT * FROM ban_lists WHERE guild_id = ?', (guild_id, ))
+    guild_lists = cur.fetchall()
+
+    message = f'**Ban lists for user {ctx.author}**\n'
+    for (ban_list_id, user_id, guild_id, timestamp) in user_lists:
+        guild = bot.get_guild(guild_id)
+        message += f'{ban_list_id}, {ctx.author}, {guild}, {timestamp}\n'
+    await ctx.send(message)
+
+    message = f'**Ban lists for guild {ctx.guild}**\n'
+    for (ban_list_id, user_id, guild_id, timestamp) in guild_lists:
+        user = bot.get_user(user_id)
+        message += f'{ban_list_id}, {user}, {ctx.guild}, {timestamp}\n'
+    await ctx.send(message)
 
 
 @bot.command()
