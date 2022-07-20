8e382db8178751e6962f3845492888382cd3110a
Frans de Boer <frans-de-boer@hotmail.com>
2022-07-20 14:38:40 +0200

    basic discord bot

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def main():
-    print('discord.ban-transfer')
+import discord
+
+from config.config import TOKEN
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    pass
+
+
+@client.event
+async def on_message(message):
+    await message.channel.send('hi!')
+
 
 if __name__ == '__main__':
-    main()
+    client.run(TOKEN)
