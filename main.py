844cb2c5cbe0117833802a450446e34e6e93c2e3
Frans de Boer <frans-de-boer@hotmail.com>
2022-07-20 14:43:48 +0200

    barebones bot complete

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from config.config import TOKEN
 bot = commands.Bot(command_prefix='!')
 
 @bot.command()
-async def say(ctx, *, arg):
-    await ctx.send(arg)
+@commands.is_owner()
+async def ping(ctx):
+    await ctx.send(f'Pong! {bot.latency:.2f} seconds')
 
 
 if __name__ == '__main__':
